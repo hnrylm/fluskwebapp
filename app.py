e14bc7d7aa3db8ac9de485c7317a270f0f30d204
hnrylm <learnubuntuandprogramming@gmail.com>
2026-03-11 00:46:39 +0800

    create templates/index.html in app.py route home to index.html

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template  # 記得導入 render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hello from Localhost via Cloudflare!</h1><p>Your Flask app is now live.</p>'
+    # 改用 render_template 來讀取 HTML 檔案
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return '<h2>這是我的第一個 Flask 網站！</h2><p>我正在學習如何將本地開發環境連接到雲端。</p>'
+    return '<h2>這是關於我頁面！</h2>'
 
 if __name__ == '__main__':
-    # Flask will run on http://127.0.0.1:5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(port=5000, debug=True)
