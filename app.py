8b4fada5791ee7eef70d2bdb36b314f92eae7084
hnrylm <learnubuntuandprogramming@gmail.com>
2026-03-11 12:23:55 +0800

    fix: resize card and image

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template  # 記得導入 render_template
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def home():
     # 改用 render_template 來讀取 HTML 檔案
     return render_template('index.html',name='Henry')
