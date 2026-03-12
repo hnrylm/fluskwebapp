cc53d46cd2c371c00894d8852238ff695cde3758
hnrylm <learnubuntuandprogramming@gmail.com>
2026-03-12 16:09:43 +0800

    c

--- app.py
+++ app.py
@@ -19,8 +19,16 @@ def index():
                 f.write(f"時間: {current_time} | 訪客: {name}\n")
             print(f"已成功保存訪客: {name}") # 在終端機顯示確認
         # -------------------
+
+        # --- 新增：讀取所有紀錄 ---
+        try:
+            with open("log.txt", "r", encoding="utf-8") as f:
+                history = f.readlines() # 讀取每一行變成一個清單
+        except FileNotFoundError:
+            history = [] # 如果檔案還沒建立，就給空清單
+        # -----------------------
         
-    return render_template('index.html', name=name)
+    return render_template('index.html', name=name,history=history)
 
 if __name__ == '__main__':
     app.run(debug=True)
